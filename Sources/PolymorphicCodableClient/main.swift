6da16717abdba23ac1d882898c5c21be7cca47aa
Pierre Marandon <pierre@phylica.fr>
2025-08-28 15:41:07 +0000

    Ajout du test.

--- Sources/PolymorphicCodableClient/main.swift
+++ Sources/PolymorphicCodableClient/main.swift
@@ -24,20 +24,37 @@ struct Flower: Plant
 struct Garden: Codable
 {
     var name: String
-    @CodableField var plant: Plant
+    @CodableField var mainPlant: Plant
 }
 
-var json1 =
+var json =
 """
 {
  "name": "Babylone",
-  "plant": 
+  "mainPlant": 
     {
       "$type": "Tree",
       "name": "Platane",
       "height": 27
-   }
+   },
+  "plants": [
+    {
+      "$type": "Flower",
+      "name": "Tulipe",
+      "petals": 8,
+      "persistence": true
+    },
+    {
+      "$type": "Tree",
+      "name": "Érable",
+      "height": 12
+    }
+  ]
 }
 """
 
-var result = try? JSONDecoder().decode(Garden.self, from: json1.data(using: .utf8)!)
+var result = try? JSONDecoder().decode(Garden.self, from: json.data(using: .utf8)!)
+
+assert(result?.name == "Babylone")
+assert(result?.mainPlant.name == "Platane")
+assert((result?.mainPlant as? Tree)?.height == 27)
